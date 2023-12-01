76600ffc983676621c6b0ef9089d87651f5e5265
Samuel <41022861+Samuel-thudi@users.noreply.github.com>
2023-12-01 15:27:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,5 +13,3 @@ class DefaultConfig:
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "40351e5d-934c-4a15-99c9-8499d98a1ca2")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "GS58Q~5pqt31koSlkxuJiNwcuOdhzIm2mnM8pca9")
-    APP_TYPE = os.environ.get("MicrosoftAppType","MultiTenant")
-    APP_TENANTID = os.environ.get("MicrosoftAppTenantId","")
